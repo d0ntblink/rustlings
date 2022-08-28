e63e59e80d13fa4e55b1c0b6d7580ce3c87ce1fa
d0ntblink <33300872+d0ntblink@users.noreply.github.com>
2022-08-27 21:46:57 -0700

    rustlings untill quiz 2, going back to book for now

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,8 +14,6 @@
 
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 // A structure to store team name and its goal details.
@@ -40,6 +38,14 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        for team_info in [(team_1_name, team_1_score, team_2_score), (team_2_name, team_2_score, team_1_score)] {
+            if let Some(team) = scores.get_mut(&team_info.0) {
+                team.goals_scored = team.goals_scored + team_info.1;
+                team.goals_conceded = team.goals_conceded + team_info.2;
+            } else {
+                scores.insert(team_info.0.clone(), Team{name: team_info.0, goals_scored: team_info.1, goals_conceded: team_info.2});
+            }
+        }
     }
     scores
 }
--- exercises/move_semantics/move_semantics4.rs
+++ exercises/move_semantics/move_semantics4.rs
@@ -4,12 +4,9 @@
 // function.
 // Execute `rustlings hint move_semantics4` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 fn main() {
-    let vec0 = Vec::new();
-
-    let mut vec1 = fill_vec(vec0);
+    let mut vec1 = fill_vec();
 
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 
@@ -20,7 +17,7 @@ fn main() {
 
 // `fill_vec()` no longer takes `vec: Vec<i32>` as argument
 fn fill_vec() -> Vec<i32> {
-    let mut vec = vec;
+    let mut vec = Vec::new();
 
     vec.push(22);
     vec.push(44);
@@ -28,3 +25,16 @@ fn fill_vec() -> Vec<i32> {
 
     vec
 }
+
+//Some errors have detailed explanations: E0061, E0423, E0425.
+// For more information about an error, try `rustc --explain E0061`.
+// hint                                                                                                                                        
+// Stop reading whenever you feel like you have enough direction :) Or try
+// doing one step and then fixing the compiler errors that result!
+// So the end goal is to:
+//    - get rid of the first line in main that creates the new vector
+//    - so then `vec0` doesn't exist, so we can't pass it to `fill_vec`
+//    - we don't want to pass anything to `fill_vec`, so its signature should
+//      reflect that it does not take any arguments
+//    - since we're not creating a new vec in `main` anymore, we need to create
+//      a new vec in `fill_vec`, similarly to the way we did in `main`
